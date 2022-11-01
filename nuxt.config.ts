2439c28c57904eb9d862ba86a5ee57eed2fe3566
Jorge <jorge@artscience.ca>
2022-11-01 15:29:38 -0400

    all of the demo is now up to date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+    modules: [
+        '@nuxtjs/i18n',
+        '@nuxtjs/tailwindcss',
+      [
+        '@storyblok/nuxt',
+        {
+                  accessToken: process.env.SB_API_TOKEN,
+                  apiOptions: { region: 'us' }
+        }
+      ]
+    ],
+    i18n: {
+        strategy: 'prefix_except_default',
+        locales: ['en', 'es'],
+        defaultLocale: 'en', // default locale
+    },
+    
+  })
